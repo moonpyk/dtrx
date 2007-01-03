241c0e50930211a028d501e7b50b561d86b2b189
brett <none@none>
2007-01-02 20:30:17 -0500

    [svn] Add basic documentation, and make this version 3.0.
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from distutils.core import setup
 
 setup(name="x",
-      version = "2.0",
+      version = "3.0",
       description = "Script to intelligently extract multiple archive types",
       author = "Brett Smith",
       author_email = "brettcsmith@brettcsmith.org",
       url = "http://www.brettcsmith.org/2006/x",
       scripts = ['scripts/x'],
+      license = "GNU General Public License, version 2 or later"
      )
